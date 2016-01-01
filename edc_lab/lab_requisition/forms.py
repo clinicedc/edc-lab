7aaaec02888ee51a69d47ab15a69a0db67ae8cf1
erikvw <ew2789@gmail.com>
2016-01-01 23:27:01 +0200

    fix incorrect super call

--- edc_lab/lab_requisition/forms.py
+++ edc_lab/lab_requisition/forms.py
@@ -6,7 +6,7 @@ from edc_constants.constants import YES, NO
 class RequisitionFormMixin(forms.ModelForm):
 
     def clean(self):
-        cleaned_data = super(RequisitionFormMixin).clean()
+        cleaned_data = super(RequisitionFormMixin, self).clean()
         if cleaned_data.get('is_drawn').lower() == YES and not cleaned_data.get('drawn_datetime'):
             raise forms.ValidationError("Date and Time Drawn must be provided if a sample was drawn.")
         if cleaned_data.get('is_drawn').lower() == NO and not cleaned_data.get('reason_not_drawn'):
