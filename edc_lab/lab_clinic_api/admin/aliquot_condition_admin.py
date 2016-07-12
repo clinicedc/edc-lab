f5eabf655fb338dd710c5b882c1f538d07021af1
Leslie <leslie.kep@gmail.com>
2016-07-12 10:54:58 +0200

    removed base model admin subclassing

--- edc_lab/lab_clinic_api/admin/aliquot_condition_admin.py
+++ edc_lab/lab_clinic_api/admin/aliquot_condition_admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 
-from .base_model_admin import BaseModelAdmin
-
 from ..models import AliquotCondition
 
 
-class AliquotConditionAdmin(BaseModelAdmin):
+class AliquotConditionAdmin(admin.ModelAdmin):
 
     list_display = ('display_index', 'name', 'short_name')
 
