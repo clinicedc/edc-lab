a257f03a2550f4e35cdac44f9080c9c8f5acf84c
erikvw <ew2789@gmail.com>
2016-11-26 18:11:27 +0200

    update faker

--- edc_lab/faker.py
+++ edc_lab/faker.py
@@ -1,14 +1,9 @@
-from faker import Faker
 from faker.providers import BaseProvider
 
 from .requisition_identifier import RequisitionIdentifier
 
 
-class EdcProvider(BaseProvider):
+class EdcLabProvider(BaseProvider):
 
     def requisition_identifier(self):
         return RequisitionIdentifier().identifier
-
-
-edc_faker = Faker()
-edc_faker.add_provider(EdcProvider)
