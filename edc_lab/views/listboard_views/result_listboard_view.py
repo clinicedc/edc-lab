ea24e7425ee4e46f0b7d98252ba481b46fbf7c5b
erikvw <ew2789@gmail.com>
2017-03-06 11:13:09 +0200

    result listboard

--- edc_lab/views/listboard_views/result_listboard_view.py
+++ edc_lab/views/listboard_views/result_listboard_view.py
@@ -2,13 +2,13 @@ from django.apps import apps as django_apps
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-from edc_base.view_mixins import EdcBaseViewMixin
-from edc_dashboard.view_mixins import AppConfigViewMixin
-from edc_dashboard.views import ListboardView
 from edc_dashboard.wrappers.model_wrapper import ModelWrapper
 
+from .base_listboard import BaseListboardView
 
-app_config = django_apps.get_app_config('edc_lab')
+
+app_name = 'edc_lab'
+app_config = django_apps.get_app_config(app_name)
 
 
 class ResultModelWrapper(ModelWrapper):
@@ -16,19 +16,16 @@ class ResultModelWrapper(ModelWrapper):
     model_name = app_config.result_model
 
 
-class ResultListboardView(AppConfigViewMixin, EdcBaseViewMixin,
-                          ListboardView):
+class ResultListboardView(BaseListboardView):
 
     app_config_name = 'edc_lab'
     navbar_item_selected = 'result'
-    navbar_name = 'specimens'
 
-    model = django_apps.get_model(*app_config.result_model.split('.'))
+    listboard_url_name = app_config.result_listboard_url_name
+    listboard_template_name = app_config.result_listboard_template_name
+    model_name = app_config.result_model
     model_wrapper_class = ResultModelWrapper
-    paginate_by = 10
-    show_all = False
-    resulted = False
-    form_action_url_name = None
+    form_action_url_name = '{}:aliquot_url'.format(app_name)
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
