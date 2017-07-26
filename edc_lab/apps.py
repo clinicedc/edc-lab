647f4cd81ddda7f66ccb03a60b83292943c365ba
erikvw <ew2789@gmail.com>
2017-07-26 17:52:10 +0200

    move templates, static to edc_lab_dashboard

--- edc_lab/apps.py
+++ edc_lab/apps.py
@@ -14,9 +14,6 @@ style = color_style()
 class AppConfig(DjangoAppConfig):
     name = 'edc_lab'
     verbose_name = 'Edc Lab'
-    admin_site_name = 'edc_lab_admin'
-
-    dashboard_name = 'edc_lab_dashboard'
 
     lab_name = 'Botswana-Harvard HIV Reference Laboratory'
     lab_address = 'Private Bag BO 320, Gaborone, Botswana'
@@ -38,39 +35,14 @@ class AppConfig(DjangoAppConfig):
     manifest_item_model = 'edc_lab.manifestitem'
     box_model = 'edc_lab.box'
 
-    aliquot_listboard_template_name = 'edc_lab/aliquot_listboard.html'
-    manage_box_listboard_template_name = 'edc_lab/manage_box_listboard.html'
-    requisition_listboard_template_name = 'edc_lab/requisition_listboard.html'
-    receive_listboard_template_name = 'edc_lab/receive_listboard.html'
-    process_listboard_template_name = 'edc_lab/process_listboard.html'
-    pack_listboard_template_name = 'edc_lab/pack_listboard.html'
-    manifest_listboard_template_name = 'edc_lab/manifest_listboard.html'
-    manage_manifest_listboard_template_name = 'edc_lab/manage_manifest_listboard.html'
-    result_listboard_template_name = 'edc_lab/result_listboard.html'
-    verify_box_listboard_template_name = 'edc_lab/verify_box_listboard.html'
-
-    home_url_name = '{}:home_url'.format(dashboard_name)
-    requisition_listboard_url_name = '{}:requisition_listboard_url'.format(
-        dashboard_name)
-    receive_listboard_url_name = '{}:receive_listboard_url'.format(dashboard_name)
-    process_listboard_url_name = '{}:process_listboard_url'.format(dashboard_name)
-    pack_listboard_url_name = '{}:pack_listboard_url'.format(dashboard_name)
-    manage_box_listboard_url_name = '{}:manage_box_listboard_url'.format(
-        dashboard_name)
-    aliquot_listboard_url_name = '{}:aliquot_listboard_url'.format(dashboard_name)
-    manifest_listboard_url_name = '{}:manifest_listboard_url'.format(dashboard_name)
-    manage_manifest_listboard_url_name = '{}:manage_manifest_listboard_url'.format(
-        dashboard_name)
-    result_listboard_url_name = '{}:result_listboard_url'.format(dashboard_name)
-    verify_box_listboard_url_name = '{}:verify_box_listboard_url'.format(
-        dashboard_name)
-
     def ready(self):
         from .models.signals import manifest_item_on_post_delete
         sys.stdout.write(f'Loading {self.verbose_name} ...\n')
         site_labs.autodiscover()
         if not self.site_code:
-            sys.stdout.write(style.NOTICE(f' * site code not defined. See AppConfig.\n'))
+            sys.stdout.write(style.NOTICE(
+                f' * site code not defined. See AppConfig.\n'))
         if not self.site_name:
-            sys.stdout.write(style.NOTICE(f' * site name not defined. See AppConfig.\n'))
+            sys.stdout.write(style.NOTICE(
+                f' * site name not defined. See AppConfig.\n'))
         sys.stdout.write(f' Done loading {self.verbose_name}.\n')
