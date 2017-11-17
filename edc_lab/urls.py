3df8f39795b354fd581b404e944bf61a23a225e0
erikvw <ew2789@gmail.com>
2017-11-17 07:28:43 +0200

    use re_path, path

--- edc_lab/urls.py
+++ edc_lab/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls.conf import path
 
 from .admin_site import edc_lab_admin
 
 app_name = 'edc_lab'
 
 urlpatterns = [
-    url(r'^admin/', edc_lab_admin.urls),
+    path('admin/', edc_lab_admin.urls),
 ]
