37b6dc0daf9907c669cabe898fb8f52d26aa7490
erikvw <ew2789@gmail.com>
2018-01-03 11:39:58 +0200

    add migration

--- edc_lab/models/__init__.py
+++ edc_lab/models/__init__.py
@@ -6,7 +6,6 @@ from .aliquot import Aliquot
 from .box import Box
 from .box_item import BoxItem
 from .box_type import BoxType
-from .identifier_history import IdentifierHistory
 from .manifest import Manifest, ManifestItem, Shipper, Consignee
 from .order import Order
 from .result import Result
