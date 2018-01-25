0589a69d88af3c80402d1b2f7a3c22e40ef45f75
erikvw <ew2789@gmail.com>
2018-01-25 08:42:28 +0200

    exclude panel from sync_models

--- edc_lab/sync_models.py
+++ edc_lab/sync_models.py
@@ -1,3 +1,3 @@
 from edc_sync.site_sync_models import site_sync_models
 
-site_sync_models.register_for_app('edc_lab')
+site_sync_models.register_for_app('edc_lab', exclude_models=['edc_lab.panel'])
