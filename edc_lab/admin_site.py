fc363ba08e32f21889bf738c98c43471ab4e2d14
erikvw <ew2789@gmail.com>
2021-01-24 21:01:20 -0600

    update for django 3.1

--- edc_lab/admin_site.py
+++ edc_lab/admin_site.py
@@ -1,12 +1,10 @@
-from django.contrib.admin import AdminSite
+from edc_model_admin.admin_site import EdcAdminSite
 
 
-class EdcLabAdminSite(AdminSite):
+class AdminSite(EdcAdminSite):
     site_header = "Edc Lab"
     site_title = "Edc Lab"
     index_title = "Edc Lab Administration"
-    site_url = "/administration/"
 
 
-edc_lab_admin = EdcLabAdminSite(name="edc_lab_admin")
-edc_lab_admin.disable_action("delete_selected")
+edc_lab_admin = AdminSite(name="edc_lab_admin")
