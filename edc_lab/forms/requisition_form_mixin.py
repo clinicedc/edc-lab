83a70df70f0433afb4a55c71c01c1e7d7011711c
erikvw <ew2789@gmail.com>
2025-02-04 21:07:06 -0600

    confirm field exists in form before setting

--- edc_lab/forms/requisition_form_mixin.py
+++ edc_lab/forms/requisition_form_mixin.py
@@ -7,8 +7,11 @@ class RequisitionFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if not kwargs.get("instance"):
-            self.fields["item_type"].initial = self.default_item_type
-            self.fields["item_count"].initial = self.default_item_count
-            self.fields["estimated_volume"].initial = self.default_estimated_volume
+            if self.fields.get("item_type"):
+                self.fields["item_type"].initial = self.default_item_type
+            if self.fields.get("item_count"):
+                self.fields["item_count"].initial = self.default_item_count
+            if self.fields.get("estimated_volume"):
+                self.fields["estimated_volume"].initial = self.default_estimated_volume
         if self.fields.get("specimen_type"):
             self.fields["specimen_type"].widget.attrs["readonly"] = True
